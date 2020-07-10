714026605c03ffa6173e1bf3c369a609a08b88f4
xuty <sunshinexuty@163.com>
2020-07-10 17:30:52 +0800

    feat:增加参数说明

--- spring-boot-rabbitmq/src/main/java/cn/xutingyin/rabbitmq/client/Recv.java
+++ spring-boot-rabbitmq/src/main/java/cn/xutingyin/rabbitmq/client/Recv.java
@@ -6,7 +6,7 @@ import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.DeliverCallback;
 
 /**
- * @description: 消费者
+ * @description: 消费者-当启动多个消费者时，会平均消费生产者生产的数据
  * @author: xuty
  * @date: 2020/6/19 10:11
  */
@@ -16,31 +16,45 @@ public class Recv {
     private final static String QUEUE_NAME = "love";
 
     public static void main(String[] argv) throws Exception {
-//        创建连接工厂
+        // 创建连接工厂
         ConnectionFactory factory = new ConnectionFactory();
-//        通过连接工厂创建一个连接
+        // 通过连接工厂创建一个连接
         Connection connection = factory.newConnection();
-//        创建一个通道
+        // 创建一个通道
         Channel channel = connection.createChannel();
         // 指定一个队列
-        // queueDeclare(String queue, boolean durable, boolean exclusive, boolean autoDelete, Map<String, Object> arguments)
+        // queueDeclare(String queue, boolean durable, boolean exclusive, boolean autoDelete, Map<String, Object>
+        // arguments)
         // 参数1 queue ：队列名
         // 参数2 durable ：是否持久化
         // 参数3 exclusive ：仅创建者可以使用的私有队列，断开后自动删除
         // 参数4 autoDelete : 当所有消费客户端连接断开后，是否自动删除队列
         // 参数5 arguments
-        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+        channel.queueDeclare(QUEUE_NAME, true, false, false, null);
         DeliverCallback deliverCallback = (consumerTag, delivery) -> {
             String message = new String(delivery.getBody(), "UTF-8");
             System.out.println(" [x] Received '" + message + "'");
+            try {
+                dowrk(message);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                // 每次处理完成一个消息后，手动发送一次应答。
+                channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
+            }
         };
         // basicConsume(String queue, boolean autoAck, Consumer callback)
         // 参数1 queue ：队列名
-        // 参数2 autoAck ： 是否自动ACK
+        // 参数2 autoAck ： 是否自动ACK -- 消费者消费了消息后会向生产者发送一个ACK,生产者收到这个ACK，才将内存中的消息删除。
         // 参数3 callback ： 消费者对象的一个接口，用来配置回调
-        channel.basicConsume(QUEUE_NAME, true, deliverCallback, consumerTag -> {
-            System.out.println("------");
+        boolean autoAck = false;
+        channel.basicConsume(QUEUE_NAME, autoAck, deliverCallback, consumerTag -> {
         });
+    }
+
+    private static void dowrk(String message) throws InterruptedException {
+        String[] taskArr = message.split(":");
+        // TimeUnit.SECONDS.sleep(Long.valueOf(taskArr[1]));
     }
 
 }
--- spring-boot-rabbitmq/src/main/java/cn/xutingyin/rabbitmq/server/Send.java
+++ spring-boot-rabbitmq/src/main/java/cn/xutingyin/rabbitmq/server/Send.java
@@ -17,12 +17,18 @@ public class Send {
         ConnectionFactory factory = new ConnectionFactory();
         try (Connection connection = factory.newConnection(); Channel channel = connection.createChannel()) {
             channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-            String message = "Hello World!";
-            channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
-            System.out.println(" [x] Sent '" + message + "'");
+            for (int i = 0; i < 10_0000; i++) {
+                String message = "Hello World:" + i;
+                // 参数1 exchange ：交换器
+                // 参数2 routingKey ： 路由键
+                // 参数3 props ： 消息的其他参数
+                // 参数4 body ： 消息体
+                channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+                System.out.println(" [x] Sent '" + message + "'");
+            }
             // 关闭频道和连接 -- 这里使用了 try resource 会自动帮我们关闭
-//            channel.close();
-//            connection.close();
+            // channel.close();
+            // connection.close();
         }
     }
 }
