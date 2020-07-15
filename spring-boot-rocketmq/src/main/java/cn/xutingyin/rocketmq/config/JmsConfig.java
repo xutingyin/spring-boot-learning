ad628a463d4a178fabf2c06ee272939eb52ec1fc
xuty <sunshinexuty@163.com>
2020-07-15 11:33:00 +0800

    feat:增加注释说明

--- spring-boot-rocketmq/src/main/java/cn/xutingyin/rocketmq/config/JmsConfig.java
+++ spring-boot-rocketmq/src/main/java/cn/xutingyin/rocketmq/config/JmsConfig.java
@@ -6,7 +6,8 @@ public class JmsConfig {
      */
     public static final String NAME_SERVER = "127.0.0.1:9876";
     /**
-     * 主题名称 主题一般是服务器设置好 而不能在代码里去新建topic（ 如果没有创建好，生产者往该主题发送消息 会报找不到topic错误）
+     * 主题名称 主题一般是服务器设置好 而不能在代码里去新建topic（ 如果没有创建好，生产者往该主题发送消息 会报找不到topic错误） -- 可以的，设置参数 autoCreateTopicEnable=true
+     * 便可以自动创建Topic
      */
     public static final String TOPIC = "topic_family";
 }
--- spring-boot-rocketmq/src/main/java/cn/xutingyin/rocketmq/consumer/Consumer.java
+++ spring-boot-rocketmq/src/main/java/cn/xutingyin/rocketmq/consumer/Consumer.java
@@ -8,6 +8,7 @@ import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
 import org.apache.rocketmq.client.exception.MQClientException;
 import org.apache.rocketmq.common.consumer.ConsumeFromWhere;
 import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.common.protocol.heartbeat.MessageModel;
 import org.springframework.stereotype.Component;
 
 import cn.xutingyin.rocketmq.config.JmsConfig;
@@ -35,8 +36,13 @@ public class Consumer {
         consumer.setNamesrvAddr(JmsConfig.NAME_SERVER);
         // 消费模式:一个新的订阅组第一次启动从队列的最后位置开始消费 后续再启动接着上次消费的进度开始消费
         consumer.setConsumeFromWhere(ConsumeFromWhere.CONSUME_FROM_LAST_OFFSET);
-        // 订阅主题和 标签（ * 代表所有标签)下信息
-        consumer.subscribe(JmsConfig.TOPIC, "*");
+        //
+        consumer.setMessageModel(MessageModel.BROADCASTING);
+        // 订阅主题和 标签（ * 代表所有标签)下信息,如果需要使用多个标签，使用 || 分隔开，例如 TagA || TagB || TagC
+        consumer.subscribe(JmsConfig.TOPIC, "TagB");
+        // 事务消息需要使用的状态
+        // LocalTransactionState.COMMIT_MESSAGE
+
         // //注册消费的监听 并在此监听中消费信息，并返回消费的状态信息
         consumer.registerMessageListener((MessageListenerConcurrently)(msgs, context) -> {
             // msgs中只收集同一个topic，同一个tag，并且key相同的message
@@ -47,6 +53,7 @@ public class Consumer {
                     // 消费者获取消息 这里只输出 不做后面逻辑处理
                     String body = new String(msg.getBody(), "utf-8");
                     log.info("Consumer-获取消息-主题topic为={}, 消费消息为={}", msg.getTopic(), body);
+                    System.out.printf("%s : %s", msg.getTopic(), body);
                 }
             } catch (UnsupportedEncodingException e) {
                 e.printStackTrace();
@@ -54,6 +61,26 @@ public class Consumer {
             }
             return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
         });
+        // 等价于上面的lambda表达式的写法
+        /* consumer.registerMessageListener(new MessageListenerConcurrently() {
+            @Override
+            public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> msgs, ConsumeConcurrentlyContext conetxt) {
+                for (Message msg : msgs) {
+        
+                    // 消费者获取消息 这里只输出 不做后面逻辑处理
+                    String body = null;
+                    try {
+                        body = new String(msg.getBody(), "utf-8");
+                        log.info("Consumer-获取消息-主题topic为={}, 消费消息为={}", msg.getTopic(), body);
+                        System.out.printf("%s : %s", msg.getTopic(), body);
+                    } catch (UnsupportedEncodingException e) {
+                        e.printStackTrace();
+                        return ConsumeConcurrentlyStatus.RECONSUME_LATER;
+                    }
+                }
+                return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+            }
+        });*/
 
         consumer.start();
         System.out.println("消费者 启动成功=======");
