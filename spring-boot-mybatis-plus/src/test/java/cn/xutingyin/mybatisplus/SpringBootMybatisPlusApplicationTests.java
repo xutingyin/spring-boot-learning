09b7c371c48b9934c140cca66213983eb4c9692c
xuty <sunshinexuty@163.com>
2020-07-17 16:42:38 +0800

    feat:提交配置文件

---
+++ spring-boot-mybatis-plus/src/test/java/cn/xutingyin/mybatisplus/SpringBootMybatisPlusApplicationTests.java
@@ -0,0 +1,41 @@
+package cn.xutingyin.mybatisplus;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+
+import cn.xutingyin.mybatisplus.entity.Student;
+import cn.xutingyin.mybatisplus.entity.StudentDto;
+import cn.xutingyin.mybatisplus.mapper.StudentMapper;
+
+@SpringBootTest
+class SpringBootMybatisPlusApplicationTests {
+
+    @Autowired
+    private StudentMapper studentMapper;
+
+    @Test
+    void queryStudents() {
+        Page<StudentDto> page = new Page<StudentDto>(1, 10);
+        IPage<StudentDto> studentDtoIPage = studentMapper.listStudentScore(page);
+        studentDtoIPage.getRecords().forEach(System.out::println);
+    }
+
+    @Test
+    void addUser() {
+        Student student = new Student();
+        student.setId(UUID.randomUUID().toString());
+        student.setAge(22);
+        student.setName("Linda");
+        student.setSex("女");
+        student.setCreateTime(LocalDateTime.now());
+        studentMapper.addUser(student);
+    }
+
+}
