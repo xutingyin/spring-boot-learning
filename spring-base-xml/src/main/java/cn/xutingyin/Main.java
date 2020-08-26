32713a1948194b12be735c82ba4ce439ee36070a
xuty <sunshinexuty@163.com>
2020-08-26 14:13:58 +0800

    fix:修改注解

--- spring-base-xml/src/main/java/cn/xutingyin/Main.java
+++ spring-base-xml/src/main/java/cn/xutingyin/Main.java
@@ -1,11 +1,7 @@
 package cn.xutingyin;
 
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.support.BeanDefinitionReader;
-import org.springframework.beans.factory.support.BeanDefinitionRegistry;
-import org.springframework.beans.factory.support.DefaultListableBeanFactory;
-import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
-import org.springframework.core.io.ClassPathResource;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * @description: Spring Bean主类测试,Spring为我们提供了丰富的应用上下文工具类：
@@ -14,7 +10,9 @@ import org.springframework.core.io.ClassPathResource;
  *               c、ClassPathXmlApplicationContext：从类路径下的一个或多个xml配置文件中加载上下文定义，把应用上下文定义文件作为类资源。
  *               d、FileSystemXMLApplicationContext：从文件系统下的一个或多个xml配置文件中加载上下文定义。
  *               e、XmlWebApplicationContext：从Web应用的一个或多个xml配置文件中加载上下文定义。
- *               其中AnnotationConfigWebApplicationContext、XmlWebApplicationContext都需要引入spring-web 这个jar包，才支持
+ *               其中AnnotationConfigWebApplicationContext、XmlWebApplicationContext都需要引入spring-web 这个jar包，才支持 BeanFactory
+ * 
+ *               注：BeanFactory在程序启动的时候，不会创建Bean的实例；只有在getBean()的时候才创建；而ApplicationContext在程序启动的时候，就进行了Bean实例的创建。
  * @author: xuty
  * @date: 2020/8/18 16:07
  */
@@ -22,19 +20,25 @@ import org.springframework.core.io.ClassPathResource;
 public class Main {
 
     public static void main(String[] args) {
-        // 基于XML 配置获取Bean
-        // ApplicationContext context = new ClassPathXmlApplicationContext("classpath:spring.xml");
+        // 基于XML 配置、使用ApplicationContext获取Bean
+        ApplicationContext context = new ClassPathXmlApplicationContext("classpath:spring.xml");
         // Human human = context.getBean("human", Human.class);
         // System.out.println(human);
         //
         // A a = context.getBean("a", A.class);
         // System.out.println(a);
-        // System.out.println(Thread.currentThread().getContextClassLoader());
-        ClassPathResource resource = new ClassPathResource("spring.xml");
-        // BeanFactory factory = new XmlBeanFactory(resource); // XmlBeanFactory已过时，官方不推荐使用-- 替代的是如下的写法
-        BeanFactory factory1 = new DefaultListableBeanFactory();
-        BeanDefinitionReader bdr = new XmlBeanDefinitionReader((BeanDefinitionRegistry)factory1);
-        bdr.loadBeanDefinitions(resource);
+        // 使用BeanFactory[这里使用具体实现类：DefaultListableBeanFactory]方式获取Bean
+        // ClassPathResource resource = new ClassPathResource("spring.xml");
+        // // BeanFactory factory = new XmlBeanFactory(resource); // XmlBeanFactory已过时，官方不推荐使用-- 替代的是如下的写法
+        // BeanFactory factory1 = new DefaultListableBeanFactory();
+        // BeanDefinitionReader bdr = new XmlBeanDefinitionReader((BeanDefinitionRegistry)factory1);
+        // bdr.loadBeanDefinitions(resource);
+        // // 获取所有加载的Bean
+        // String[] beanDefinitionNames = ((BeanDefinitionRegistry)factory1).getBeanDefinitionNames();
+        // List<String> beanNames = Arrays.asList(beanDefinitionNames);
+        // beanNames.forEach(beanName -> {
+        // System.out.println(beanName);
+        // });
         // Human human = (Human)factory1.getBean(Human.class);
         // System.out.println(human);
         // Human human = (Human)factory.getBean("human");
