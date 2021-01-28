35b679f4d6258c5232c5107c1103265a302bbdc5
xuty <sunshinexuty@163.com>
2021-01-28 11:09:41 +0800

    modify file

--- spring-data-jpa/src/test/java/cn/xutingyin/springdatajpa/SpringDataJpaApplicationTests.java
+++ spring-data-jpa/src/test/java/cn/xutingyin/springdatajpa/SpringDataJpaApplicationTests.java
@@ -2,6 +2,7 @@ package cn.xutingyin.springdatajpa;
 
 import cn.xutingyin.springdatajpa.dao.UserRepository;
 import cn.xutingyin.springdatajpa.entity.User;
+import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -23,11 +24,17 @@ class SpringDataJpaApplicationTests {
     }
 
     @Test
-    void getByCondition(){
+    void saveEntity(){
         User user = new User();
         user.setName("赵武");
         User save = userRepository.save(user);
         System.out.println(save);
+    }
+
+    @Test
+    void getById(){
+        List<User> users = userRepository.findAllById(Lists.newArrayList("1", "2"));
+        System.out.println(users);
     }
 
 
